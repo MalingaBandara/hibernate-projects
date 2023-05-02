2af419c56714c51e6dc92e877d9f90c984d901b1
Malinga Bandara <malinga1021@gmail.com>
2023-05-02 14:26:08 +0530

    inheritance default strategy

---
+++ inheritance-default-strategy/src/HibernateUtill.java
@@ -0,0 +1,43 @@
+import entity.Animal;
+import entity.Cat;
+import entity.Fish;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+public class HibernateUtill {
+
+    private static StandardServiceRegistry standardServiceRegistry;
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            if (sessionFactory == null) {
+                standardServiceRegistry
+                        = new StandardServiceRegistryBuilder()
+                        .configure()
+                        .build();
+
+                MetadataSources metadataSources = new MetadataSources(standardServiceRegistry)
+                        .addAnnotatedClass(Animal.class)
+                        .addAnnotatedClass(Fish.class)
+                        .addAnnotatedClass(Cat.class);
+
+                Metadata metadata = metadataSources.getMetadataBuilder().build();
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+            }
+        } catch (Exception e) {
+            if (standardServiceRegistry != null) {
+                StandardServiceRegistryBuilder.destroy(standardServiceRegistry);
+            }
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
